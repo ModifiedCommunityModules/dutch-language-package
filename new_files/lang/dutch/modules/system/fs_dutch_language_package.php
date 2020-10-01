e82e6bd3e5617e3dcd27bf988318f3b8f90f98ce
php-schubser <d.bjelajac@hoqmee.net>
2020-10-01 14:52:47 +0200

    feat: install language automatically

---
+++ new_files/lang/dutch/modules/system/fs_dutch_language_package.php
@@ -0,0 +1,11 @@
+<?php
+define('MODULE_FS_DUTCH_LANGUAGE_PACKAGE_TITLE', '<img src="../lang/dutch/admin/images/icon.gif" alt="Nederlands" title="Nederlands" style="border:0;"> Nederlandstalig pakket © by <a href="https://www.shopbetreuung.com/" target="_blank" style="color: #007bff; font-size: inherit; font-weight: inherit;">FishnetServices</a>');
+define('TEXT_LANGUAGE_STATUS', 'Status van het taalpakket: %s');
+define('TEXT_LANGUAGE_STATUS_ADMIN', 'Taalpakket status admin: %s');
+define('TEXT_LANGUAGE_CONFIGURATION', 'U kunt de configuratie voor de taal <a href="languages.php"><strong>hier</strong></a> wijzigen.');
+define('TEXT_LANGUAGE_DESRIPTION', 'Nederlandstalig pakket voor Gewijzigd vanaf versie 2.0.5.0.');
+define('TEXT_ACTIVE', '<strong style="color:green;">Actief</strong>');
+define('TEXT_INACTIVE', '<strong style="color:red;">Inactief</strong>');
+define('TEXT_LANGUAGE_INSTALLED_SUCCESSFULLY', 'De Nederlandse taal werd met succes ge&#xEF;nstalleerd.  Als u inhoud uit andere talen in deze taal wilt kopi&#xEB;ren, kunt u dat <a style="font-size:12px; color:green;" href="languages.php"><strong>hier</strong></a> doen.');
+define('TEXT_DELETE_LANGUAGE_CONFIRM', 'Wilt u de module verwijderen?<br /><br /><b>WAARSCHUWING</b>: Alle gegevens voor artikelen, categorie&#xEB;n en inhoud worden ook in deze taal gewist!');
+define('TEXT_DELETE_LANGUAGE_DEFAULT_CONFIRM', '<b>WAARSCHUWING</b>: Je kunt deze module niet verwijderen, want dit is de standaard taal van de winkel.');
